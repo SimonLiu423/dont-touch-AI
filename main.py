cdc60b1aa4b7c9f66549b570ac06931ed258ec18
SimonLiu <simonliu423@gmail.com>
2023-05-21 21:31:08 +0800

    Fix: move entry point of training to main.py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-import pygame
-from src import MazeCar
-
-from src.env import FPS
-from mlgame.view.view import PygameView
-from mlgame.game.generic import quit_or_esc
-
-SOUND_OFF = "off"
-
-MOVE_MAZE = "MOVE_MAZE"
+from rl.train import *
 
 if __name__ == '__main__':
-    pygame.init()
-    # game = MazeCar.MazeCar(6, "MAZE", 6, 10000, 3, "off")
-    # game = MazeCar.MazeCar(user_num=2, game_type=MOVE_MAZE, map=5, time_to_play=2000, sensor_num=3, sound=SOUND_OFF)
-    game = MazeCar.MazeCar(2, "PRACTICE", 12, 10000, 5, "off")
-    scene_init_info_dict = game.get_scene_init_data()
-    game_view = PygameView(scene_init_info_dict)
-    frame_count = 0
-    while game.is_running and not quit_or_esc():
-        pygame.time.Clock().tick_busy_loop(FPS)
-        game.update(game.get_keyboard_command())
-        game_progress_data = game.get_scene_progress_data()
-        game_view.draw(game_progress_data)
-        frame_count += 1
+    N_MAPS = 12
+    FRAME_LIMIT = 1800
 
-    pygame.quit()
+    ai_name = "1P"
+    mlplay = MLPlay(ai_name)
+
+    while True:
+        map_id = np.random.randint(1, N_MAPS)
+        game = Dont_touch(1, map_id, FRAME_LIMIT, 5, "off")
+        print("Training with map {}".format(map_id))
+        while game.is_running and not quit_or_esc():
+            scene_info = game.get_data_from_game_to_player()[ai_name]
+            commands = {ai_name: mlplay.update(scene_info)}
+
+            #            if scene_info["status"] in ("GAME_OVER", "GAME_PASS"):
+            #                mlplay.reset()
+            #                break
+            game.update(game.get_keyboard_command())
+
--- rl/train.py
+++ rl/train.py
@@ -93,23 +93,3 @@ class MLPlay:
             torch.save(self.agent.target_net.state_dict(), model_path)
 
 
-if __name__ == '__main__':
-    N_MAPS = 12
-    FRAME_LIMIT = 1800
-
-    ai_name = "1P"
-    mlplay = MLPlay(ai_name)
-
-    while True:
-        map_id = np.random.randint(1, N_MAPS)
-        game = Dont_touch(1, map_id, FRAME_LIMIT, 5, "off")
-        print("Training with map {}".format(map_id))
-        while game.is_running and not quit_or_esc():
-            scene_info = game.get_data_from_game_to_player()[ai_name]
-            commands = {ai_name: mlplay.update(scene_info)}
-
-#            if scene_info["status"] in ("GAME_OVER", "GAME_PASS"):
-#                mlplay.reset()
-#                break
-            game.update(game.get_keyboard_command())
-
